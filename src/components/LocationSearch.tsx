5813d41e06c563a7d5e057bbf8fe7e519aafc2e0
radu-bordea <radu.bordea.dev@gmail.com>
2024-04-16 11:19:49 +0200

    implement search, connect to api and add interfaces for places and searchresponse

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 import type { Place } from "../api/Place";
+import { search } from "../api/search";
 
 interface LocationSearchProps {
   onPlaceClick: (place: Place) => void;
 }
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [term, setTerm] = useState("");
   const [places, setPlaces] = useState<Place[]>([]);
 
-  return <div>Search for the location</div>;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await search(term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
