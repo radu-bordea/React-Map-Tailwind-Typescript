a3817ac62b5dec1a22cd99f588ff02939748374f
radu-bordea <radu.bordea.dev@gmail.com>
2024-04-21 10:50:32 +0200

    change containers width

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,12 @@ function App() {
     <div className="h-screen bg-gray-700 text-blue-100  w-screen grid grid-cols-12">
       {/* Main container with 12 columns */}
       {/* Column for LocationSearch component */}
-      <div className="col-span-3 p-2">
+      <div className="col-span-4 p-2">
         {/* Spanning 3 columns with padding */}
         <LocationSearch onPlaceClick={(p) => setPlace(p)} /> {/* Rendering the LocationSearch component and passing a function to handle place click event */}
       </div>
       {/* Column for Map component */}
-      <div className="col-span-9">
+      <div className="col-span-8">
         {/* Spanning 9 columns */}
         <Map place={place} /> {/* Rendering the Map component and passing the selected place */}
       </div>
